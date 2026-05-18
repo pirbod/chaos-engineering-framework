eb53d4453f31439750a4442df796306e0ffb9c49
PARHAM MOOSAVI <pirbod@me.com>
2026-05-18 16:48:16 +0200

    Fix CI workflow failures

--- terraform/modules/azure-lab/main.tf
+++ terraform/modules/azure-lab/main.tf
@@ -8,7 +8,6 @@ resource "azurerm_dev_test_lab" "this" {
   name                = "${var.prefix}-${var.environment}-lab"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
-  lab_storage_type    = "Standard"
   tags                = merge(var.tags, { environment = var.environment })
 }
 
